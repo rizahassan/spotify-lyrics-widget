3217fd92f9944739afe19aac69a7955032cc066f
Riza Hassan <55040840+rizahassan@users.noreply.github.com>
2021-07-05 22:18:18 -0500

    Added python shell commands

--- src/connector.js
+++ src/connector.js
@@ -1,17 +1,97 @@
+
 function changeText() {
+    // Display/Hide the get lyric button
+    var lyricButton = document.getElementById("lyric-button");
+    if (lyricButton.style.display === "none") {
+        lyricButton.style.display = "block";
+      } else {
+        lyricButton.style.display = "none";
+      }
+
   const { PythonShell } = require("python-shell");
 
-  let pyshell = new PythonShell('./backend/script.py');
+  let artist = {
+    mode: "text",
+    pythonOptions: ["-u"], // get print results in real-time
+    args: ["artist"],
+  };
 
-  pyshell.on('message',function(message){
-      console.log(message);
-      document.getElementById("text").innerHTML = message;
-  })
+  let song = {
+    mode: "text",
+    pythonOptions: ["-u"], // get print results in real-time
+    args: ["song"],
+  };
 
-  pyshell.end(function(err){
-      if(err){
-          throw err;
-      };
-      console.log('finished');
+  let lyric = {
+    mode: "text",
+    pythonOptions: ["-u"], // get print results in real-time
+    args: ["lyric"],
+  };
+
+  let artistCommand = new PythonShell("./backend/script.py", artist);
+  let songCommand = new PythonShell("./backend/script.py", song);
+  let lyricCommand = new PythonShell("./backend/script.py", lyric);
+
+  artistCommand.on("message", function (message) {
+    console.log(message);
+    document.getElementById("artist-name").innerHTML = message;
   });
+
+  artistCommand.end(function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("Artist command finished");
+  });
+
+  songCommand.on("message", function (message) {
+    console.log(message);
+    document.getElementById("song-title").innerHTML = message;
+  });
+
+  songCommand.end(function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("Song command finished");
+  });
+
+  lyricCommand.on("message", function (message) {
+    console.log(message);
+    document.getElementById("lyrics").innerHTML = message;
+  });
+
+  lyricCommand.end(function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("Lyric command finished");
+  });
+
+  
 }
+
+// function authenticate(){
+//     const { PythonShell } = require("python-shell");
+
+//     let authenticate = {
+//         mode: "text",
+//         pythonOptions: ["-u"], // get print results in real-time
+//         args: ["authenticate"],
+//       };
+
+//     let authenticateCommand = new PythonShell("./backend/script.py", authenticate);
+
+//     authenticateCommand.on("message", function (message) {
+//     console.log(message);
+//     });
+
+//     authenticateCommand.end(function (err) {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log("Lyric command finished");
+//     });
+    
+
+// }
